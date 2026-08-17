e92fd1491ebe651f3b664ef0983a365772514796
Mike <119491615+Smail3737@users.noreply.github.com>
2026-08-17 12:22:59 +0300

    Restrict SSH access to specific IP address

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,6 @@ resource "aws_security_group" "demo" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["203.0.113.10/32"]
   }
 }
